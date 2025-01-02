835b02f7fe28e3939ddaa951a05045799d66824a
Akash Shaw <shawakash2003@gmail.com>
2025-01-03 01:33:40 +0530

    chore: component changes

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,50 @@ use yew::prelude::*;
 #[function_component]
 fn Counter() -> Html {
     let counter = use_state(|| 0);
-    let onclick = {
+    let onclick_increment = {
         let counter = counter.clone();
         move |_| {
             counter.set(*counter + 1);
         }
     };
 
+    let onclick_decrement = {
+        let counter = counter.clone();
+        move |_| {
+            if *counter == 0 {
+                return;
+            }
+            counter.set(*counter - 1);
+        }
+    };
+
     html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
+        <div class="space-y-4 w-full flex flex-col items-center">
+            <div class="flex justify-between w-3/5">
+                <button
+                    onclick={onclick_increment}
+                    class="bg-[#0551cf] text-white px-4 py-2 rounded-md hover:bg-[#0442a7] transition-colors"
+                >
+                    { "Increment" }
+                </button>
+                <button
+                    onclick={onclick_decrement}
+                    class="bg-[#0551cf] text-white px-4 py-2 rounded-md hover:bg-[#0442a7] transition-colors"
+                >
+                    { "Decrement" }
+                </button>
+            </div>
+            <p class="text-center text-2xl font-bold">{ *counter }</p>
+        </div>
+    }
+}
+
+#[function_component]
+fn Hero() -> Html {
+    html! {
+        <div class="text-center">
+            <h1 class="text-4xl font-bold text-gray-800 mb-2">{"Hello, Yew!"}</h1>
+            <p class="text-gray-600">{" This is my first application in yew! "}</p>
         </div>
     }
 }
@@ -21,12 +54,12 @@ fn Counter() -> Html {
 #[function_component]
 fn App() -> Html {
     html! {
-        <div>
-            <h1>{"Hello, Yew!"}</h1>
-            <p>{" This is my first application in yew! "}</p>
-            <br />
-            <Counter />
-        </div>
+        <main class="min-h-screen bg-[#fafaf7] flex items-center justify-center">
+            <div class="max-w-md w-full mx-auto p-8 space-y-8">
+                <Hero />
+                <Counter />
+            </div>
+        </main>
     }
 }
 
